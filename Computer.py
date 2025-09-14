6131cc1f719f7b6701667888f19731afbaeb4bb2
melsaqui <melanie.wayuaquino@gmail.com>
2025-09-14 16:53:18 +0800

    Computer.py for single-player games using minimax

---
+++ Computer.py
@@ -0,0 +1,306 @@
+
+from Player import Player
+from functools import lru_cache
+from threading import Thread
+from threading import Event
+from threading import Lock
+
+import copy
+import time
+
+class Computer(Player):
+    def __init__(self,role,game,enemy):
+        super().__init__(role,game)
+        self.enemy=enemy
+    
+    def get_possible_moves(self,board):
+        possible_moves=[]
+        
+        #center
+        size=self.game.size
+
+        for i in range(size):
+            for j in range(size):
+                #cell=self.board_rep
+                if board[j][i]!="_" :
+                    for x in range(-3,4):
+                        for y in range(-3,4):
+                            ref_x=i+x
+                            ref_y=j+y
+                            if size>ref_x>=0 and size>ref_y>=0:
+                                ref_coord=[ref_x,ref_y]
+                                ref_cell=board[ref_y][ref_x]
+                                if  ref_cell=="_" and ref_coord not in possible_moves:
+                                    possible_moves.append([ref_x,ref_y])
+        possible_moves.sort(
+            key=lambda move: self.move_score_to_sort(move),
+            reverse=True
+        )
+        
+        return possible_moves
+    def move_score_to_sort(self,cell):
+        x=cell[0]
+        y=cell[1]
+        score=0
+        size=self.game.size
+        dis_cen =abs(x-int(size/2)) + abs(y-int(size/2))
+        score-=dis_cen
+
+        if (x, y) in [(0, 0), (0, size - 1), (size - 1, 0), (size - 1, size - 1)]:
+            score += 3      
+        for dx in [-1, 0, 1]:
+            for dy in [-1, 0, 1]:
+                nx, ny = x + dx, y + dy 
+                if 0 <= nx < size and 0 <= ny < size:
+                    if self.board_rep[ny][nx]!= "_":
+                        score+=5
+                    
+        return score
+    
+    def evaluate(self,board,role,enemy):
+        counter_diag1 =0
+        counter_diag2=0
+        stop_count_diag1=False
+        stop_count_diag2=False
+        higher_row=-1
+        higher_col=-1
+
+        for i in range(len(board)):
+            counter_col=0
+            counter_row =0
+            stop_count_col=False
+            stop_count_row=False
+
+            for j in range(len(board[0])):
+                if board[i][j] == role and not stop_count_row:
+                    counter_row+=1
+                elif board[i][j] == enemy:
+                    counter_row=0
+                    stop_count_row=True
+
+                if board[j][i] == role and not stop_count_col:
+                    counter_col+=1
+                elif board[j][i] == enemy:
+                    counter_col =0
+                    stop_count_col=True
+                if stop_count_col and stop_count_row:
+                    break
+            if board[i][i]== role and not stop_count_diag1:
+                counter_diag1+=1
+            elif board[i][i] == enemy:
+                stop_count_diag1 =True
+                counter_diag1 =0
+
+            if  board[i][self.game.size-1-i] == role and not stop_count_diag2:
+                counter_diag2+=1     
+            elif board[i][self.game.size-1-i] == enemy:
+                stop_count_diag2 =True
+                counter_diag2 =0 
+
+            if counter_row>higher_row:
+                higher_row=counter_row
+            if  counter_col>higher_col:
+                higher_col=counter_col
+      
+        score = max(higher_col,higher_row,counter_diag2,counter_diag1)
+        if score == self.game.size:
+            return self.game.size**5
+        if score == self.game.size-1:
+            return score**3
+        if score >= int(self.game.size/2) +1:
+            return score**2
+        return score
+            
+    hash_table={}
+    
+    #@lru_cache(maxsize=None)
+    def minimax(self,board,depth,alpha,beta,isMax,max_depth):
+        #key = (tuple(map(tuple, board)),depth,isMax)
+        #if key in self.hash_table:
+        #    return self.hash_table[key]
+        
+        possible_moves=self.get_possible_moves(board)
+        win_score=self.game.size**5
+        one_away =(self.game.size-1)**3
+        max_score= self.evaluate(board,self.role,self.enemy.role)
+        min_score= self.evaluate(board,self.enemy.role,self.role)
+        actual_enemy =self.evaluate(self.board_rep,self.enemy.role,self.role)
+  
+        if max_score==win_score:
+           #self.terminal_found =True
+            self.stop_event.set() 
+            return max_score-depth
+        elif min_score==win_score:
+            self.stop_event.set() 
+            return depth-min_score
+        elif len(possible_moves) ==0:
+            self.stop_event.set() 
+            return 0
+        elif depth==max_depth and (actual_enemy==one_away) and len(possible_moves)!=0:
+            print(f"max depth changed: {max_depth}")
+            max_depth+=1
+        elif depth==max_depth:
+            if max_score>min_score:
+                return max_score -depth
+            elif max_score<=min_score:
+                return depth-(min_score)
+            
+        if self.stop_event.is_set():
+            return float("-inf"),"aborted"
+        if isMax:
+            best_score =float("-inf")
+            if self.stop_event.is_set()==False:
+                for move in possible_moves:
+                    board[move[1]][move[0]]=self.role
+                    score = self.minimax(board,depth + 1,alpha,beta,False,max_depth)
+                
+                    board[move[1]][move[0]]="_"
+                    if not isinstance(score,tuple):
+                        best_score = max(score, best_score)
+                        alpha= max(best_score,alpha)
+                        if(beta<=alpha):
+                            break
+                    else: pass
+
+                #self.hash_table[key] = best_score
+                return best_score
+        elif not isMax:
+            best_score = float("inf")
+            if self.stop_event.is_set()==False:
+
+                for move in possible_moves:
+                    board[move[1]][move[0]]=self.role
+                    score = self.minimax(board,depth + 1,alpha,beta,True,max_depth)
+                    board[move[1]][move[0]] = "_"
+                    if not isinstance(score,tuple):
+
+                        best_score = min(score, best_score)
+                        beta = min(best_score,beta)
+                        if(beta<=alpha):
+                            break
+                        return best_score
+                #self.hash_table[key] = best_score
+                    else: return score
+                #return best_score
+        
+    score_move =[]
+
+    terminal_found=False
+    results_lock = Lock()
+
+    def get_best_move(self,move,board):
+        alpha= float("-inf")
+        beta= float("inf")
+            #return  
+        if self.stop_event.is_set():
+            return
+        board[move[1]][move[0]] = self.role
+        score= self.minimax(board,0,alpha,beta,True,5)
+        
+        board[move[1]][move[0]] = "_"
+        with self.results_lock:
+            if not isinstance(score,list):
+                self.score_move.append([score,move])
+    
+    def split_possible_move(self, possible_moves):  
+        threads = []
+        self.stop_event = Event()
+
+        for move in (possible_moves):
+            t = Thread(target=self.get_best_move, args=(move,copy.deepcopy(self.board_rep),))
+            threads.append(t)
+            t.start()
+
+        for t in threads:
+            t.join()
+        best_score=float("-inf")
+        print("arr contents")
+        print(self.score_move)
+        for score,move in self.score_move:
+            if score!=None and best_score<score :
+                best_score=score
+                best_move=move
+        print("Best move")
+        print(best_move)
+        self.arr=[]
+        self.terminal_found =False
+        return best_score,best_move
+    def corners(self):
+        size=self.game.size
+        half_size =int(size/2)
+        center_cell=self.board_rep[half_size][half_size]#self.game.get_cell_by_axis(half_size,half_size)
+        corner1_cell=self.board_rep[0][0]
+        corner2_cell =self.board_rep[size-1][0]
+        corner3_cell =self.board_rep[0][size-1]
+        corner4_cell=self.board_rep[size-1][size-1]
+
+        if corner1_cell =="_" and corner4_cell == self.enemy:
+            return 0,0
+        elif corner2_cell =="_" and corner3_cell ==self.enemy:
+            return 0,size-1
+        elif corner3_cell =="_" and corner2_cell ==self.enemy:
+            return size-1,0
+        elif corner4_cell=="_" and corner1_cell ==self.enemy:
+            return size-1,size-1
+        elif center_cell=="_" and (corner1_cell ==self.enemy.role or corner2_cell==self.enemy.role or corner3_cell==self.enemy.role or corner4_cell ==self.enemy.role): 
+            return half_size,half_size
+        elif corner1_cell=="_" and center_cell ==self.enemy:
+            return 0,0
+        elif corner2_cell=="_" and center_cell ==self.enemy:
+            return 0,size-1
+        elif corner3_cell=="_" and  center_cell ==self.enemy:
+            return size-1,0
+        elif corner4_cell=="_" and center_cell ==self.enemy:
+            return size-1,size-1
+        
+        elif corner1_cell=="_":
+            return 0,0
+        elif corner2_cell=="_":
+            return 0,size-1
+        elif corner3_cell=="_":
+            return size-1,0
+        elif corner4_cell=="_":
+            return size-1,size-1
+    def move_process(self):
+        start_time = time.perf_counter()
+        self.get_board_rep()
+        print(f"Current enemy score: {self.evaluate(self.board_rep,self.enemy.role,self.role)}")
+
+        corner_move =self.corners()
+        if corner_move!=None:
+            best_move=self.game.get_cell_by_axis(corner_move[0],corner_move[1])
+        else:   
+            possible_moves=self.get_possible_moves(self.board_rep)
+            print("Possible moves")
+            print (possible_moves)
+            data=self.split_possible_move(possible_moves)
+            move=data[1]
+            best_move=self.game.get_cell_by_axis(move[0],move[1])
+        
+        best_move.val = self.role
+        best_move.mark_cell()
+        end_time = time.perf_counter()
+        duration =end_time-start_time
+        print(f"processing time: {int(duration//3600)}h {int(duration//60)}m {duration%60}s")
+    def get_board_rep(self):
+        self.board_rep=[]
+        for i in range(self.game.size):
+            row = []
+            for j in range(self.game.size):
+                cell=self.game.get_cell_by_axis(j,i)
+                if cell.val=="":
+                    row.append("_")
+                else:
+                    row.append(cell.val)   
+            self.board_rep.append(row)
+    def move(self):
+        self.best_move=""
+        func_thread = Thread(target=self.move_process)
+        func_thread.start()
+    
+        status_thread = Thread(target=self.display)
+        status_thread.start()
+        #func_thread.join()
+        #status_thread.join()
+    def display(self):
+        self.game.heading.configure(text="Waiting for Computer to play...")
